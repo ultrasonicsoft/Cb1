c4dbe5b9ed07032ee6c6b2554d3108dfb4878e0d
ultrasonicsoft <balramchavan@gmail.com>
2014-07-17 09:50:33 +0530

    engine config

---
+++ Chessbot/EngineConfigurationSettings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace OpenCVDemo1
+{
+      [Serializable()]
+    public class EngineConfigurationSettings
+    {
+        public int ContemptFactorValue { get; set; }
+
+        public int MinSplitDepthValue { get; set; }
+
+        public int ThreadValue { get; set; }
+
+        public int HashValue { get; set; }
+
+        public int MultiPVValue { get; set; }
+
+        public bool Ponder { get; set; }
+
+        public int SkillLevelValue { get; set; }
+
+        public int MoveHorizonValue { get; set; }
+
+        public int BaseTimeValue { get; set; }
+
+        public int MoveTimeValue { get; set; }
+
+        public int ThinkingTimeValue { get; set; }
+
+        public int SlowMoverValue { get; set; }
+
+        public bool UC_960 { get; set; }
+        
+    }
+}
