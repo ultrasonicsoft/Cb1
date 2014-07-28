c861365adcf751c1e12392bbbdced197a7536c6a
ultrasonicsoft <balramchavan@gmail.com>
2014-07-28 23:32:04 +0530

    added

---
+++ Chessbot/Helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+
+namespace OpenCVDemo1
+{
+    public class Helper
+    {
+        public static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+    }
+}
